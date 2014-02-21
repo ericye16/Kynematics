c2c692defac30c5c3db9fe79ca0f98497cfba9f7
Eric Ye <ericye16@gmail.com>
2014-02-21 16:58:49 -0500

    more work on engine, gah sensors

--- src/com/ericye16/android/kynematics/Engine.java
+++ src/com/ericye16/android/kynematics/Engine.java
@@ -1,43 +1,84 @@
 package com.ericye16.android.kynematics;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Environment;
+import android.text.format.Time;
 
 public class Engine implements SensorEventListener {
 	private boolean isCollecting = false;
+	private final Activity activity;
+	private SensorManager sensorManager;
+	private Sensor accel;
+	private BufferedOutputStream dataStream;
 	
-	public Engine() {
-		//I suppose something should happen here
+	public Engine(Activity activity) {
+		this.activity = activity;
+		sensorManager = (SensorManager) activity.getSystemService(Activity.SENSOR_SERVICE);
+		accel = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 	}
 	
 	public boolean isCollecting() {
 		return isCollecting;
 	}
 	
-	public void startOrStop() {
+	public void startOrStop() throws IOException {
 		if (isCollecting) {
 			stop();
 		} else {
-			start();
+			start();			
 		}
+		isCollecting = !isCollecting;
 	}
 	
-	private void start() {
-		
+	private void start() throws IOException {
+		if (!isExternalStorageWritable()) throw new IOException();
+		Time time = new Time();
+		time.setToNow();
+		String timeString = time.toString();
+		File dataFile = new File(Environment.getExternalStorageDirectory(), timeString + ".csv");
+		dataStream = new BufferedOutputStream(new FileOutputStream(dataFile));
+		sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
 	}
 	
 	private void stop() {
+		sensorManager.unregisterListener(this);
+		try {
+			dataStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/* Checks if external storage is available for read and write */
+	private boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int arg1) {
 		
 	}
 
 	@Override
-	public void onAccuracyChanged(Sensor arg0, int arg1) {
-		
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent arg0) {
-		
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		switch (sensorEvent.sensor.getType()) {
+		case Sensor.TYPE_LINEAR_ACCELERATION:
+			break;
+		default:
+			break;
+		}
 	}
 }
